dfdb3ab2fc76c47609d2d5112a310f3e13c87424
venanziocichella <venanzio.cichella@gmail.com>
2021-06-11 19:21:31 -0500

    modified composite example
    modified composite example

--- testcomposite.m
+++ testcomposite.m
@@ -1,11 +1,30 @@
+clear all
 close all
 
-Cp = [0 -2 3 3 1 0 11 0 3 15];
-tnodes = [0 3 5 7 10];
-time = linspace(0,10,1000);
 
-poly_t = CompositeBernsteinPoly(Cp,tnodes,time);
 
-plot(linspace(tnodes(1),tnodes(end),length(poly_t)),poly_t);
-grid on
+f = @(t) 1./(sqrt(1+(12-t).^2));
+t = 0:0.01:12
+
+N = 41;
+tnodes = linspace(t(1),t(end),N+1);
+Bf = BernsteinPoly(f(tnodes),t);
+
+tknots = [0 8 11 12];
+N = 10;
+M = length(tknots) - 1; 
+tnodes = zeros(1,N*M+1);
+for i = 1 : M
+    tnodes(1,(i-1)*N+1:i*N+1) = linspace(tknots(i),tknots(i+1),N+1);
+end
+CoBf = CompositeBernsteinPoly(f(tnodes),tknots,t)
+
+
+plot(t,f(t),'Color','b'); hold on
+plot(t,Bf,'Color','r');
+plot(t,CoBf,'Color','g');
+
+
+
+% grid on
 
