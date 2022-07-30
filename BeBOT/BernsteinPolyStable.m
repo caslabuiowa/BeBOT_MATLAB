c43972deb7ae016b26f556ef60ddaf129b61b15d
Venanzio Cichella <33523003+venanziocichella@users.noreply.github.com>
2022-07-30 03:23:55 -0500

    Update BernsteinPolyStable.m
    notational change

--- BeBOT/BernsteinPolyStable.m
+++ BeBOT/BernsteinPolyStable.m
@@ -2,10 +2,10 @@ function  poly_t  = BernsteinPolyStable(Cp,t)
 
 [m,n] = size(Cp);
 poly_t = zeros(m,length(t));
-count = 0
+count = 0;
 for i = 1 : length(t)
     [~,poly_t(:,i)] = deCasteljau(Cp,t(i));
-    count = count + 1
+    count = count + 1;
 end
 
 end
